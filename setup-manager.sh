2c767fa4c73aff9dfb36fc970e1b1f7b126b1f1c
vlad.polkhovsky <vlad.polkhosky@gmail.com>
2025-07-10 12:03:49 +0000

    update

---
+++ setup-manager.sh
@@ -0,0 +1,45 @@
+apt install ufw
+apt install git
+
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow ssh
+ufw allow 5000/tcp
+
+ufw allow 2377/tcp
+ufw allow 7946/tcp
+ufw allow 7946/udp
+ufw allow 4789/udp
+
+ufw allow 8080/tcp
+ufw allow 8080/udp
+
+ufw enable
+ufw reload
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+#создать /etc/docker/daemon.json
+#{
+#  "insecure-registries": ["185.159.128.138:5000"]
+#}
+sudo systemctl restart docker
+
+docker run -d -p 5000:5000 --restart always --name registry --volume registry-volume registry:latest
+
+git clone https://github.com/vladpolkhovsky/swarn-k8s-example-microservice.git
+
+cd swarn-k8s-example-microservice
+
+docker build --tag swarn-k8s-example-microservice .
+docker tag swarn-k8s-example-microservice 185.159.128.138:5000:5000/swarn-k8s-example-microservice
+docker push 185.159.128.138:5000:5000/swarn-k8s-example-microservice
+
+#Инициализация кластера
+docker swarm init --advertise-addr 185.159.128.138
+
+#Узнать токен для подключения воркера
+docker swarm join-token worker
+
+docker stack deploy -c docker-compose-swarn.yml swarn-example-app
