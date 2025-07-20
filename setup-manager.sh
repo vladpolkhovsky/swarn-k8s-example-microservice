efcfc56bd77c920046e5719e7942b7ba4ac49f5f
vlad.polkhovsky <vlad.polkhovsky@gmail.com>
2025-07-20 13:31:34 +0300

    update for k8s

--- setup-manager.sh
+++ setup-manager.sh
@@ -1,32 +1,5 @@
-apt install ufw
-apt install git
-
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow 5000/tcp
-
-ufw allow 2377/tcp
-ufw allow 7946/tcp
-ufw allow 7946/udp
-ufw allow 4789/udp
-
-ufw allow 8080/tcp
-ufw allow 8080/udp
-
-ufw enable
-ufw reload
-
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-
-#создать /etc/docker/daemon.json
-#{
-#  "insecure-registries": ["185.159.128.138:5000"]
-#}
-sudo systemctl restart docker
-
-docker run -d -p 5000:5000 --restart always --name registry --volume registry-volume registry:latest
 
 git clone https://github.com/vladpolkhovsky/swarn-k8s-example-microservice.git
 
